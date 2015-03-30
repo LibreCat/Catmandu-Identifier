6e9681ed2a3057500ca051e37350928f2b81038d
vpeil <vitali.peil@uni-bielefeld.de>
2015-03-30 14:38:53 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# NAME
+
+Catmandu::Identifier - Blah blah blah
+
+# SYNOPSIS
+
+    use Catmandu::Identifier;
+
+# DESCRIPTION
+
+Catmandu::Identifier is
+
+# AUTHOR
+
+Vitali Peil <vitali.peil@uni-bielefeld.de>
+
+# COPYRIGHT
+
+Copyright 2015- Vitali Peil
+
+# LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# SEE ALSO
---
+++ lib/Catmandu/Identifier.pm
@@ -0,0 +1,39 @@
+package Catmandu::Identifier;
+
+use strict;
+use 5.008_005;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Catmandu::Identifier - Blah blah blah
+
+=head1 SYNOPSIS
+
+  use Catmandu::Identifier;
+
+=head1 DESCRIPTION
+
+Catmandu::Identifier is
+
+=head1 AUTHOR
+
+Vitali Peil E<lt>vitali.peil@uni-bielefeld.deE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2015- Vitali Peil
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
