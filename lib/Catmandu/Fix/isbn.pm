d530c4557e60ab351e6095e4ccdf0e55a296d205
vpeil <vitali.peil@uni-bielefeld.de>
2015-05-29 07:07:04 +0200

    documentation added (closes #1)

--- lib/Catmandu/Fix/isbn.pm
+++ lib/Catmandu/Fix/isbn.pm
@@ -6,12 +6,21 @@ use Moo;
 use Catmandu::Fix::Has;
 
 has path => (fix_arg => 1);
+has length => (fix_arg => 1, default => sub{ 10 });
 
 with 'Catmandu::Fix::SimpleGetValue';
 
 sub emit_value {
   my ($self, $var) = @_;
-  "${var} = Business::ISBN->new(${var})->as_string if is_value(${var});";
+
+  my $len = $self->length;
+  if ($len == 10) {
+    "${var} = Business::ISBN->new(${var})->as_isbn10->as_string if is_value(${var});";
+  } elsif ($len == 13) {
+    "${var} = Business::ISBN->new(${var})->as_isbn13->as_string if is_value(${var});";
+  } else {
+    Catmandu::BadArg->throw("Second (optional) argument must be either 10 or 13.\n");
+  }
 }
 
 =head1 NAME
@@ -20,8 +29,18 @@ Catmandu::Fix::isbn - normalize the isbn value of a key
 
 =head1 SYNOPSIS
 
-# Normalize the ISBN value of foo. E.g. foo => ''
-isbn(foo) # foo => ''
+  # Normalize the ISBN value of isbn_field.
+  # e.g. isbn_field => '1565922573'
+
+  isbn(isbn_field) # isbn_field => '1-56592-257-3'
+
+  # same as
+
+  isbn(isbn_field, 10) # isbn_field => '1-56592-257-3'
+
+  # isbn in 13 digits form
+
+  isbn(isbn_field, 13) # isbn_field => '978-1-56592-257-0'
 
 =head1 SEE ALSO
 
@@ -30,3 +49,27 @@ L<Catmandu::Fix>
 =cut
 
 1;
+
+__END__
+
+use Catmandu::Sane;
+use Business::ISBN;
+use Moo;
+use Catmandu::Fix::Has;
+
+has path => (fix_arg => 1);
+has length => (fix_arg => 1, default => sub{ 10 });
+has obj => (is => 'lazy');
+
+with 'Catmandu::Fix::SimpleGetValue';
+
+sub emit_value {
+  my ($self, $var) = @_;
+  my $len = $self->length;
+
+  if($len == 10) {
+    return "${var} = Business::ISBN->new(${var})->as_isbn10->as_string if is_value(${var});";
+  } else {
+    return "${var} = Business::ISBN->new(${var})->as_isbn13->as_string if is_value(${var});";
+  }
+}
